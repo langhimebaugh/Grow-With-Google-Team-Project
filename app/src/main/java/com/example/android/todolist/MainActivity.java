0bae59fa1c7fbb68753355e2f414c195d3d92e09
Langdon Himebaugh <lang@himebaugh.com>
2018-03-02 15:47:01 -0500

    Connect app to Firebase Add Realtime Databse - Dependencies set up. Add Firebase Authentication - Dependencies set up. google-services.json left out.

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -32,6 +32,11 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.android.todolist.data.TaskContract;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity implements
@@ -62,6 +67,34 @@ public class MainActivity extends AppCompatActivity implements
         // Initialize the adapter and attach it to the RecyclerView
         mAdapter = new CustomCursorAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
+
+
+        // *************************************************************
+        // Write to FIREBASE
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        // *************************************************************
+
 
         /*
          Add a touch helper to the RecyclerView to recognize when a user swipes to delete an item.
